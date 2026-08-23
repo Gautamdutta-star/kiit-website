1bc2ccf17fc4f056011b17f6b1b3e903cbcf657b
Gautam Dutta <duttagautam39@gmail.com>
2026-08-24 05:15:17 +0530

    Refactor database connection to use environment variables

--- save_student.php
+++ save_student.php
@@ -1,6 +1,12 @@
 <?php
 
-$conn = new mysqli("localhost","root","","college_db");
+$conn = new mysqli(
+    getenv('MYSQLHOST'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLDATABASE'),
+    getenv('MYSQLPORT')
+);
 
 if($conn->connect_error){
     die("DB Error");
