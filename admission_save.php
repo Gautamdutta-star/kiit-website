f1b2a6f5a40278d532e57b85c51c3e0ad7988895
Gautam Dutta <duttagautam39@gmail.com>
2026-08-24 17:29:30 +0530

    Update database connection method in admission_save.php
    Refactor database connection to use MYSQL_URL environment variable.

--- admission_save.php
+++ admission_save.php
@@ -1,11 +1,14 @@
 <?php
-$conn = new mysqli(
-    getenv('MYSQLHOST'),
-    getenv('MYSQLUSER'),
-    getenv('MYSQLPASSWORD'),
-    getenv('MYSQLDATABASE'),
-    (int) getenv("MYSQLPORT")
-);
+$url = getenv('MYSQL_URL');
+$db = parse_url($url);
+
+$host = $db['host'];
+$user = 'root';
+$password = urldecode($db['pass']);
+$database = ltrim($db['path'], '/');
+$port = $db['port'] ?? 3306;
+
+$conn = new mysqli($host, $user, $password, $database, $port);
 if($conn->connect_error){
     die("DB Error: " . $conn->connect_error);
 }
